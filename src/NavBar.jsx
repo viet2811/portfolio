deca33cd9f9d58e73f8b2213c9648fae2bbd1f50
vietanh28 <hoangvietanh730@gmail.com>
2025-04-26 23:58:22 +0100

    Update new navigation bar style

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,19 +1,33 @@
 function NavBar() {
     return (
-        <nav className="w-screen fixed top-0 bg-[#0a192f] overflow-hidden max-h-15">
-            <a href="#" className="float-right text-[#8f99b6] 
-                                   decoration-0 p-3.5 hover:text-[#f5f5f5] mr-3">
-                <b className="text-[#5ae1d4]">02.</b> Project
-            </a>
-            <a href="#work-experience" className="float-right text-[#8f99b6] 
-                                   decoration-0 p-3.5 hover:text-[#f5f5f5]">
-                <b className="text-[#5ae1d4]">01.</b> Work Experience
-            </a>
-            <a href="#about-me" className="float-right text-[#8f99b6] 
-                                   decoration-0 p-3.5 hover:text-[#f5f5f5]">
-                <b className="text-[#5ae1d4]">00.</b> About me
-            </a>
+        <nav className="flex w-screen fixed top-0 bg-deepnavy h-10 justify-end pr-5" aria-label="Breadcrumb">
+            <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
+                <li className="inline-flex items-center">
+                    <a href="#about-me" className="inline-flex items-center text-sm font-medium
+                     text-gray-700 hover:text-blue-600 dark:text-gray-400 dark:hover:text-white">
+                        <b>00.</b>&nbsp;About me</a>
+                </li>
+                <li>
+                    <div className="flex items-center">
+                        <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                        <a href="#work-experience" className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2
+                         dark:text-gray-400 dark:hover:text-white">
+                         <b>01.</b> Experience</a>
+                    </div>
+                </li>
+                <li aria-current="page">
+                    <div className="flex items-center">
+                        <svg className="rtl:rotate-180 w-3 h-3 text-gray-400 mx-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 9 4-4-4-4" />
+                        </svg>
+                        <span className="ms-1 text-sm font-medium text-gray-500 md:ms-2 dark:text-gray-400"><b>02.</b> Projects</span>
+                    </div>
+                </li>
+            </ol>
         </nav>
+
     );
 }
 
--- src/SkillList.jsx
+++ src/SkillList.jsx
@@ -1,6 +1,6 @@
 function SkillCard ({name}) {
     return (
-        <div className="bg-[#193755] text-[#55d7d4] w-max px-4 py-1.5 rounded-3xl mr-2.5 text-sm">
+        <div className="bg-[#193755] text-[#55d7d4] w-max px-4 py-1 rounded-full mr-2.5 text-sm">
             {name}
         </div>
     );
